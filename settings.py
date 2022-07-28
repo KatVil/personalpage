c2dd2b379c64194175c0eafc7ab67e275ce68cf1
Katherine Vilinski-Mazur <89085481+KatVil@users.noreply.github.com>
2022-07-28 22:51:41 +0300

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,19 @@
+import php_heroku
+Debug =  bool( os.environ.get('PHP_DEBUG', False) )
+ALLOWED_HOSTS = ['*']
+MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware', #Добавляем до 'django.middleware.security.SecurityMiddleware',
+]
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'email@gmail.com' #С которого будет приходить сообщение
+EMAIL_HOST_PASSWORD = 'password'
+EMAIL_PORT = 587
+
+ADMINS = [
+    ('Kat', 'katvilinskimazur@gmail.com'),
+]
+WHITENOISE_USE_FINDERS = True
+STATIC_ROOT = None
+php_heroku.settings(locals()) #В конце
